27477812ff1ae02174f5c0676b35196de453043e
Matt <72781410+SSummer-UI@users.noreply.github.com>
2021-09-21 04:53:18 +0900

    LARGEUPDATE

--- index.js
+++ index.js
@@ -1,110 +1,29 @@
-'use strict';
+const RPC = require("discord-rpc");
+const rpc = new RPC.Client({
+    transport: "ipc"
+})
 
-const Discord = require('discord-rpc');
-const EventEmitter = require('events');
-
-const browser = typeof window !== 'undefined';
-
-function makeClient(clientId) {
-  const rpc = new Discord.Client({ transport: browser ? 'websocket' : 'ipc' });
-
-  let connected = false;
-  let activityCache = null;
-
-  const instance = new class RP extends EventEmitter {
-    updatePresence(d) {
-      if (connected) {
-        rpc.setActivity(d).catch((e) => this.emit('error', e));
-      } else {
-        activityCache = d;
-      }
-    }
-
-    reply(user, response) {
-      const handle = (e) => this.emit('error', e);
-      switch (response) {
-        case 'YES':
-          rpc.sendJoinInvite(user).catch(handle);
-          break;
-        case 'NO':
-        case 'IGNORE':
-          rpc.closeJoinRequest(user).catch(handle);
-          break;
-        default:
-          throw new RangeError('unknown response');
-      }
-    }
-
-    disconnect() {
-      rpc.destroy().catch((e) => this.emit('error', e));
-    }
-  }();
-
-  rpc.on('error', (e) => instance.emit('error', e));
-
-  rpc.login({ clientId })
-    .then(() => {
-      instance.emit('connected');
-      connected = true;
-
-      rpc.subscribe('ACTIVITY_JOIN', ({ secret }) => {
-        instance.emit('join', secret);
-      });
-      rpc.subscribe('ACTIVITY_SPECTATE', ({ secret }) => {
-        instance.emit('spectate', secret);
-      });
-      rpc.subscribe('ACTIVITY_JOIN_REQUEST', (user) => {
-        instance.emit('joinRequest', user);
-      });
-
-      if (activityCache) {
-        rpc.setActivity(activityCache).catch((e) => instance.emit('error', e));
-        activityCache = null;
-      }
+rpc.on("ready", () => {
+    rpc.setActivity({
+        details: "Editing Sequence 2",
+        state: "Untitled.vpj",
+        largeImageKey: "image",
+        largeImageText: "Using Videopad Pro v 8.63",
+        state: "Playing Solo",
+        details: "Competitive",
+        startTimestamp: Date.now(),
+        endTimestamp: 1633117826000,
+        largeImageText: "Numbani",
+        smallImageText: "Rogue - Level 100",
+        partyId: "ae488379-351d-4a4f-ad32-2b9b01c91657",
+        partySize: 1,
+        partyMax: 5,
+        joinSecret: "MTI4NzM0OjFpMmhuZToxMjMxMjM= ",
     })
-    .catch((e) => instance.emit('error', e));
 
-  return instance;
-}
+    console.log("RPC active");
+})
 
-module.exports = makeClient;
-
-// ----------------------------------------------------------------
-
-const client = require('.')('180984871685062656');
-
-client.on('join', (secret) => {
-  console.log('we should join with', secret);
-});
-
-client.on('spectate', (secret) => {
-  console.log('we should spectate with', secret);
-});
-
-client.on('joinRequest', (user) => {
-  if (user.discriminator === '1337') {
-    client.reply(user, 'YES');
-  } else {
-    client.reply(user, 'IGNORE');
-  }
-});
-
-client.on('connected', () => {
-  console.log('connected!');
-
-  client.updatePresence({
-    state: 'slithering',
-    details: '🐍',
-    startTimestamp: new Date(),
-    largeImageKey: 'snek_large',
-    smallImageKey: 'snek_small',
-    partyId: 'snek_party',
-    partySize: 1,
-    partyMax: 1,
-    matchSecret: 'slithers',
-    joinSecret: 'boop',
-    spectateSecret: 'sniff',
-  });
-});
-
-process.on('unhandledRejection', console.error);
+rpc.login({
+    clientId: "776550128071999519"
+})
